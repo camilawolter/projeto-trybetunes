eed0c0c43c5e3b795d8d19ee6ddff4f30a6bab20
Camila Wolter <camila0wolter@gmail.com>
2022-08-01 19:33:05 -0300

    requisito 5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ class Header extends React.Component {
     return (
       <header data-testid="header-component">
         <nav>
-          <Link to="/search" data-testid="link-to-search">Pesquisa</Link>
+          <Link to="/search" data-testid="link-to-search">Pesquisar</Link>
           <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
           <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
         </nav>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,55 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nameArtist: '',
+      isDisabled: true,
+    };
+  }
+
+  handleChange = ({ name, value }) => {
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.EnableButton();
+    });
+  }
+
+  EnableButton = () => {
+    const { nameArtist } = this.state;
+    const MIN_CARACTER = 2;
+    const validateForCaracters = nameArtist.length >= MIN_CARACTER;
+    this.setState({
+      isDisabled: !validateForCaracters,
+    });
+  }
+
   render() {
+    const { nameArtist, isDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <h2>Search</h2>
         <Header />
+        <form>
+          <input
+            type="text"
+            name="nameArtist"
+            value={ nameArtist }
+            data-testid="search-artist-input"
+            placeholder="Nome do Artista"
+            onChange={ ({ target }) => this.handleChange(target) }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isDisabled }
+          >
+            Pesquisar
+
+          </button>
+        </form>
       </div>
     );
   }
